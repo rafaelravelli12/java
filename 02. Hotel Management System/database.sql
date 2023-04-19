d180b615af1a30ad9586a6978da5909d5e9709c9
rafaelravelli12 <rafaelravelli1@gmail.com>
2023-04-19 00:52:21 -0300

    Update database.sql

--- 02. Hotel Management System/database.sql
+++ 02. Hotel Management System/database.sql
@@ -31,7 +31,10 @@ CREATE TABLE IF NOT EXISTS `customer` (
   `deposit` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table w_project_02.customer: ~0 rows (approximately)
+-- Dumping data for table w_project_02.customer: ~2 rows (approximately)
+INSERT INTO `customer` (`id`, `number`, `name`, `gender`, `country`, `room_number`, `status`, `deposit`) VALUES
+	('Passport', '2', 'Emily', 'Female', 'Russia', '2', 'Yes', '75'),
+	('Passport', '3', 'Tim Burt', 'Male', 'Nepal', '8', 'Yes', '120');
 
 -- Dumping structure for table w_project_02.driver
 CREATE TABLE IF NOT EXISTS `driver` (
@@ -44,7 +47,10 @@ CREATE TABLE IF NOT EXISTS `driver` (
   `location` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table w_project_02.driver: ~0 rows (approximately)
+-- Dumping data for table w_project_02.driver: ~2 rows (approximately)
+INSERT INTO `driver` (`name`, `age`, `gender`, `company`, `brand`, `available`, `location`) VALUES
+	('Paul Willianms', '44', 'Male', 'Peugeot', '207', 'Yes', 'A Park'),
+	('Davis Mills', '35', 'Male', 'Audi', 'c3', 'Yes', 'B Park');
 
 -- Dumping structure for table w_project_02.employee
 CREATE TABLE IF NOT EXISTS `employee` (
@@ -58,9 +64,11 @@ CREATE TABLE IF NOT EXISTS `employee` (
   `email` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table w_project_02.employee: ~1 rows (approximately)
+-- Dumping data for table w_project_02.employee: ~3 rows (approximately)
 INSERT INTO `employee` (`name`, `age`, `gender`, `job`, `salary`, `phone`, `aadhar`, `email`) VALUES
-	('Lúcio', '35', 'male', 'Front Desk Clerks', '2000', '11966301521', '554', 'lucio@gmail.com');
+	('James Smith', '35', 'male', 'Front Desk Clerks', '2000', '11966301521', '554', 'lucio@gmail.com'),
+	('Robert Brown', '30', 'male', 'Manager', '4000', '11966301599', '1231', 'robert@gmail.com'),
+	('David Miller', '40', 'male', 'Kitchen Staff', '2500', '+551195540039', '123331', 'david@gmail.com');
 
 -- Dumping structure for table w_project_02.login
 CREATE TABLE IF NOT EXISTS `login` (
@@ -68,7 +76,7 @@ CREATE TABLE IF NOT EXISTS `login` (
   `password` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table w_project_02.login: ~0 rows (approximately)
+-- Dumping data for table w_project_02.login: ~1 rows (approximately)
 INSERT INTO `login` (`username`, `password`) VALUES
 	('admin', '12345');
 
@@ -81,7 +89,18 @@ CREATE TABLE IF NOT EXISTS `room` (
   `bed_type` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table w_project_02.room: ~0 rows (approximately)
+-- Dumping data for table w_project_02.room: ~10 rows (approximately)
+INSERT INTO `room` (`room_number`, `availability`, `clean_status`, `price`, `bed_type`) VALUES
+	('1', 'Available', 'Used', '100', 'Single Bed'),
+	('2', 'Occupied', 'Used', '100', 'Single Bed'),
+	('3', 'Available', 'Cleaned', '100', 'Single Bed'),
+	('4', 'Available', 'Cleaned', '100', 'Single Bed'),
+	('5', 'Available', 'Cleaned', '100', 'Single Bed'),
+	('6', 'Available', 'Cleaned', '180', 'Double Bed'),
+	('7', 'Available', 'Cleaned', '180', 'Double Bed'),
+	('8', 'Occupied', 'Used', '180', 'Double Bed'),
+	('9', 'Available', 'Cleaned', '180', 'Double Bed'),
+	('10', 'Available', 'Cleaned', '180', 'Double Bed');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
