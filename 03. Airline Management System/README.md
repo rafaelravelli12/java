0a90292d455f8fc0e77ebf7881577cbcff84f1a6
rafaelravelli12 <rafaelravelli1@gmail.com>
2023-04-24 23:54:49 -0300

    Project_03 - database and readme updated

---
+++ 03. Airline Management System/README.md
@@ -0,0 +1,30 @@
+# Airline Management System
+
+### Estimeed Sirs,
+
+This repository is being used to allocate my Applied Projects based on Udemy Course "45 Real World Java Development Projects Bootcamp Course 2022", https://www.udemy.com/course/java-programming-project-using-jsp-jdbc-servlets/.
+
+------------
+
+This software was developed in Windows 11 Environment: https://www.microsoft.com/en-us/windows/windows-11?r=1
+
+This software was developed in Java JDK 17: https://www.oracle.com/br/java/technologies/downloads/#jdk17-windows
+
+This software was developed in Apache NetBeans IDE 17: https://netbeans.apache.org/download/index.html
+
+This software was developed in MySQL Server 8.0: https://dev.mysql.com/downloads/installer/
+
+This software was developed in HeidiSQL database administration tool: https://www.heidisql.com/
+
+------------
+
+Required commands to run the Project:
+
+- Run folder /Airline-Management-System on NetBeans;
+- Run database.sql as a HeidiSQL query;
+- Add /Airline-Management-System/mysql-connector-java-8.0.27.jar and /Airline-Management-System/rs2xml.jar Libraries;
+- 'Run file' Airline-Management-System/src/hotel/Login.java on NetBeans;
+- Login: admin
+  Password: 12345
+
+Graciously, 
--- 03. Airline Management System/database.sql
+++ 03. Airline Management System/database.sql
@@ -19,29 +19,6 @@
 CREATE DATABASE IF NOT EXISTS `w_project_03` /*!40100 DEFAULT CHARACTER SET latin1 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `w_project_03`;
 
--- Dumping structure for table w_project_03.cancellation
-CREATE TABLE IF NOT EXISTS `cancellation` (
-  `pnr_no` varchar(50) DEFAULT NULL,
-  `cancellation_no` varchar(50) DEFAULT NULL,
-  `cancellation_date` varchar(50) DEFAULT NULL,
-  `fli_code` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Dumping data for table w_project_03.cancellation: ~0 rows (approximately)
-
--- Dumping structure for table w_project_03.flight
-CREATE TABLE IF NOT EXISTS `flight` (
-  `f_code` varchar(50) DEFAULT NULL,
-  `f_name` varchar(50) DEFAULT NULL,
-  `src` varchar(50) DEFAULT NULL,
-  `dst` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Dumping data for table w_project_03.flight: ~0 rows (approximately)
-INSERT INTO `flight` (`f_code`, `f_name`, `src`, `dst`) VALUES
-	('AI266', 'AIR INDIA-9', 'PATNA', 'DELHI'),
-	('AI267', 'AIR INDIA-10', 'PATNA', 'DELHI');
-
 -- Dumping structure for table w_project_03.flight_info
 CREATE TABLE IF NOT EXISTS `flight_info` (
   `company` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
@@ -50,14 +27,15 @@ CREATE TABLE IF NOT EXISTS `flight_info` (
   `date` varchar(50) DEFAULT NULL,
   `time` varchar(50) DEFAULT NULL,
   `code` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
+  `plane_model` varchar(50) DEFAULT NULL,
   `remaining_executive_chairs` varchar(50) DEFAULT NULL,
   `remaining_popular_chairs` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Dumping data for table w_project_03.flight_info: ~2 rows (approximately)
-INSERT INTO `flight_info` (`company`, `source`, `destination`, `date`, `time`, `code`, `remaining_executive_chairs`, `remaining_popular_chairs`) VALUES
-	('TAM', 'São Paulo - Congonhas', 'Rio de Janeiro - Santos Dummont', '25/05/2023', '07:00', 'A001', NULL, NULL),
-	('TAM', 'São Paulo - Guarulhos', 'Rio de Janeiro - Santos Dummont', '25/05/2023', '07:00', 'A001', NULL, NULL);
+INSERT INTO `flight_info` (`company`, `source`, `destination`, `date`, `time`, `code`, `plane_model`, `remaining_executive_chairs`, `remaining_popular_chairs`) VALUES
+	('TAM', 'São Paulo - Guarulhos', 'Rio de Janeiro - Santos Dummont', '25/05/2023', '07:00', 'A001', 'BOEING 777-200ER/LR', '99', '189'),
+	('TAM', 'São Paulo - Congonhas', 'Rio de Janeiro - Santos Dummont', '25/05/2023', '07:00', 'A002', 'BOEING 747-400', '150', '220');
 
 -- Dumping structure for table w_project_03.login
 CREATE TABLE IF NOT EXISTS `login` (
@@ -65,37 +43,9 @@ CREATE TABLE IF NOT EXISTS `login` (
   `password` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table w_project_03.login: ~0 rows (approximately)
+-- Dumping data for table w_project_03.login: ~1 rows (approximately)
 INSERT INTO `login` (`username`, `password`) VALUES
 	('admin', '12345');
-
--- Dumping structure for table w_project_03.passenger
-CREATE TABLE IF NOT EXISTS `passenger` (
-  `pnr_no` varchar(50) DEFAULT NULL,
-  `address` varchar(50) DEFAULT NULL,
-  `nationality` varchar(50) DEFAULT NULL,
-  `name` varchar(50) DEFAULT NULL,
-  `gender` varchar(50) DEFAULT NULL,
-  `ph_no` varchar(50) DEFAULT NULL,
-  `passport_no` varchar(50) DEFAULT NULL,
-  `fl_code` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Dumping data for table w_project_03.passenger: ~0 rows (approximately)
-INSERT INTO `passenger` (`pnr_no`, `address`, `nationality`, `name`, `gender`, `ph_no`, `passport_no`, `fl_code`) VALUES
-	('a500', 'Joaquim Tàvora', 'Brasil', 'Rafael', 'male', '458893872', '1000', '1');
-
--- Dumping structure for table w_project_03.payment
-CREATE TABLE IF NOT EXISTS `payment` (
-  `pnr_no` varchar(50) DEFAULT NULL,
-  `ph_no` varchar(50) DEFAULT NULL,
-  `cheque_no` varchar(50) DEFAULT NULL,
-  `card_no` varchar(50) DEFAULT NULL,
-  `paid_amt` varchar(50) DEFAULT NULL,
-  `pay_date` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Dumping data for table w_project_03.payment: ~0 rows (approximately)
 
 -- Dumping structure for table w_project_03.plane
 CREATE TABLE IF NOT EXISTS `plane` (
@@ -104,42 +54,23 @@ CREATE TABLE IF NOT EXISTS `plane` (
   `popular_class_capacity` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table w_project_03.plane: ~0 rows (approximately)
+-- Dumping data for table w_project_03.plane: ~3 rows (approximately)
 INSERT INTO `plane` (`model`, `executive_class_capacity`, `popular_class_capacity`) VALUES
 	('BOEING 777-200ER/LR', '100', '190'),
 	('BOEING 747-400', '150', '220'),
 	('BOEING 737-800', '50', '110');
 
--- Dumping structure for table w_project_03.reservation
-CREATE TABLE IF NOT EXISTS `reservation` (
-  `pnr_no` varchar(50) DEFAULT NULL,
-  `ticket_id` varchar(50) DEFAULT NULL,
-  `f_code` varchar(50) DEFAULT NULL,
-  `jny_date` varchar(50) DEFAULT NULL,
-  `jny_time` varchar(50) DEFAULT NULL,
-  `src` varchar(50) DEFAULT NULL,
-  `dst` varchar(50) DEFAULT NULL
+-- Dumping structure for table w_project_03.ticket
+CREATE TABLE IF NOT EXISTS `ticket` (
+  `passenger_name` varchar(50) DEFAULT NULL,
+  `fight_code` varchar(50) DEFAULT NULL,
+  `chair_class` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table w_project_03.reservation: ~0 rows (approximately)
-INSERT INTO `reservation` (`pnr_no`, `ticket_id`, `f_code`, `jny_date`, `jny_time`, `src`, `dst`) VALUES
-	('1', '1', '1', '1', '1', 'PATNA', 'BANGALORE'),
-	('2', '2', '2', '2', '2', 'BANGALORE', 'MUMBAI'),
-	('a', 'a', 'a', 'a', 'a', 'a', 'a'),
-	('12345', 'ID01', 'AI266', '01/01/2021', '20:10', 'PATNA', 'DELHI');
-
--- Dumping structure for table w_project_03.sector
-CREATE TABLE IF NOT EXISTS `sector` (
-  `f_code` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
-  `capacity` varchar(50) DEFAULT NULL,
-  `class_code` varchar(50) DEFAULT NULL,
-  `class_name` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Dumping data for table w_project_03.sector: ~0 rows (approximately)
-INSERT INTO `sector` (`f_code`, `capacity`, `class_code`, `class_name`) VALUES
-	('AI266', '20', 'A', 'First Class'),
-	('AI267', '20', 'A', 'First Class');
+-- Dumping data for table w_project_03.ticket: ~2 rows (approximately)
+INSERT INTO `ticket` (`passenger_name`, `fight_code`, `chair_class`) VALUES
+	('Dale Nelson', 'A001', 'Executive'),
+	('Britney Jordan', 'A001', 'Popular');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
